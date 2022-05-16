26e770d326315af6c82114d01f87f927567d3cb9
monkey90 <monkey901109@gmail.com>
2022-05-16 16:09:33 +0900

    Revert "- 수정본 올리기"
    This reverts commit ba7edd533adc6bf0cc76d0cb0e95ac54265a5c3f.

--- client/pages/NFT/MyNFTData.js
+++ client/pages/NFT/MyNFTData.js
@@ -4,9 +4,8 @@ import kip17Abi from "../../components/kip17Abi";
 import Mystyles from "../../styles/mynft.module.css";
 import FireBaseInit from '../../components/FireBaseInit';
 
-
 const MyNFTData = ({ caver, newKip17addr }) => {
-  const [nftlist, setNftlist] = useState([]);
+  const [nftlist, setNftlist] = useState([]);  
   const [GameNFTlist, setGameNFTlist] = useState([]);
   const [Showlist, setShowlist] = useState([]);
   const [isGameNFT, setGameNFT] = useState(false);
@@ -20,84 +19,21 @@ const MyNFTData = ({ caver, newKip17addr }) => {
 
   useEffect(() => {
     SaveMyToken();
-    KasMigrate();
-    /*
-    TokenHistoryQueryOptions options = new TokenHistoryQueryOptions();
-    options.setStatus("completed");
-    options.setSize((long)1);
-    options.setType("KIP-7");
-
-    PageableFtContractDetails details = caver.kas.tokenHistory.getFTContractList(options);
-    System.out.println(details);
-    */
-
-
-
+    
   }, []);
 
-  /*
-  void getContractList() {
-    try {
-        Kip17ContractListResponse response = caver.kas.kip17.getContractList();
-    } catch(ApiException e) {
-        //handle error
-    }
-  }
-  */
-
-  const KasMigrate = async () => {
-    const CaverExtKAS = require('caver-js-ext-kas');
-    const caverExt = new CaverExtKAS();
-
-    const account = 'KASKY2SYRNZ8X68SL3QKRT4C';
-    const privateKey = '8AMfKLWAzy8pSALU5Emo_iijrX7K6dqOPezqOH7x';
-    const chainId = '1001';
-    caverExt.initKASAPI(chainId, account, privateKey);
-    console.log("Init KIP17 API");
-
-    const contracts = await caverExt.kas.kip17.getContractList();
-    console.log("GetContractList : " + JSON.stringify(contracts));
-
-    const query = {
-      status: caverExt.kas.tokenHistory.queryOptions.status.COMPLETED,
-      size: 100,
-      type: caverExt.kas.tokenHistory.queryOptions.type.KIP17,
-  }
-    const result = await caverExt.kas.tokenHistory.getNFTContractList(query); 
-    console.log("getNFTContractList : " + JSON.stringify(result));
-
-    /* 
-    // Create a KeyringContainer instance
-    const keyringContainer = new caverExt.keyringContainer();
-
-    // Create a keyring from private key
-
-    const keyring = keyringContainer.keyring.createFromPrivateKey(privateKey);
-
-    // Add a keyring to the keyringContainer
-    keyringContainer.add(keyring);
-    */
-
-
-    //const address = keyring.address;
-    //const key = keyring.key.privateKey;
-    //const nonce = 0;
-
-    //const ret = await caver.kas.wallet.migrateAccounts([{ address, key, nonce }]);
-    //console.log(ret); 
-  }
-
-  const SettingGameNFT = (isOK) => {
+  const SettingGameNFT = (isOK) =>
+  {
     setGameNFT(isOK);
-    if (isOK == true)
+    if(isOK == true)
       setShowlist(GameNFTlist);
     else
       setShowlist(nftlist);
-
+      
   }
 
 
-  const SaveMyToken = async () => {
+  const SaveMyToken = async () =>{
     const tokenContract = "";
     const account = window.sessionStorage.getItem('ID');
 
@@ -127,20 +63,19 @@ const MyNFTData = ({ caver, newKip17addr }) => {
       //let blocknumber = await caver.rpc.klay.getBlockByNumber(tokenId);
       //let blockhash = await caver.rpc.klay.getFilterLogs(account);
       //console.log("blockhash : " + JSON.stringify(blockhash));
-
+      
       //transaction.getTransactionHash()
       //transaction.getSenderTxHash()
-
-      if (String(tokenOwner).toLowerCase() === account) {
+      
+      if (String(tokenOwner).toLowerCase() === account) 
+      {
         let tokenURI = await tokenContract.methods.tokenURI(tokenId).call();
         FireBaseDB = false;
         console.log("tokenURI : " + tokenURI);
-
-        //Nft nft = caver.kas.tokenHistory.getNFT(contractAddress, tokenId);
-        //System.out.println(nft);
-
-        const URL = tokenURI.substring(0, 7);
-        if (URL == "ipfs://") {
+        
+        const URL = tokenURI.substring(0,7);
+        if (URL == "ipfs://")
+        {
           const MetaDataJson = tokenURI.replace("ipfs://", "https://gateway.pinata.cloud/ipfs/");
 
           //const config = { headers : { 'Accept' : 'application/json'}};
@@ -161,45 +96,47 @@ const MyNFTData = ({ caver, newKip17addr }) => {
           const NFTItem = FireBaseInit.collection("NFT_ITEM");
 
           NFTItem.doc(JsonName).get().then((doc) => {
-            if (doc.exists) {
+            if(doc.exists)
+            { 
               console.log("파이어베이스 이제 들어옴");
 
               FireBaseDB = true;
               setGameNFTlist((prevState) => {
-                return [...prevState, { name, symbol, tokenId, JsonURL, JsonName, JsonDescription, FireBaseDB }];
-              });
+                return [...prevState, {name, symbol, tokenId, JsonURL, JsonName, JsonDescription, FireBaseDB }];
+              });        
             }
           });
         }
-        else {
+        else
+        {
           JsonName = tokenURI;
         }
 
         setNftlist((prevState) => {
-          return [...prevState, { name, symbol, tokenId, JsonURL, JsonName, JsonDescription, FireBaseDB }];
+          return [...prevState, {name, symbol, tokenId, JsonURL, JsonName, JsonDescription, FireBaseDB }];
         });
 
         setShowlist((prevState) => {
-          return [...prevState, { name, symbol, tokenId, JsonURL, JsonName, JsonDescription, FireBaseDB }];
+          return [...prevState, {name, symbol, tokenId, JsonURL, JsonName, JsonDescription, FireBaseDB }];
         });
       }
     }
 
-  };
-
+  };  
+ 
 
   return (
     <div>
       <MainTitle />
       <div>
-        <button type="button" className="btn btn-outline-dark" onClick={() => { SettingGameNFT(false) }}>ALL NFT</button>
-        <button type="button" className="btn btn-outline-dark" onClick={() => { SettingGameNFT(true) }}>Game NFT</button>
+      <button type="button" className="btn btn-outline-dark" onClick = {() => {SettingGameNFT(false)}}>ALL NFT</button>
+      <button type="button" className="btn btn-outline-dark" onClick = {() => {SettingGameNFT(true)}}>Game NFT</button>
       </div>
       {Showlist.map((token) => {
         return (
-          <form className={Mystyles.todoNFTShowtemplate} key={token.id}>
+          <form className={Mystyles.todoNFTShowtemplate} key={token.id}> 
             <div className="card mb-3">
-              <h3 className="card-header">{token.FireBaseDB ? token.name + " GameNFT" : token.name}</h3>
+              <h3 className="card-header">{ token.FireBaseDB ? token.name + " GameNFT" : token.name}</h3>
               <div className="card-body">
                 <img
                   src={token.JsonURL}
